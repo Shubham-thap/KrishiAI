1fdbfc7db812c66fd2467fd224279a37cd34c568
Shubham <shubhamthapa2022@gmail.com>
2024-01-15 13:33:16 +0545

    third

--- app.py
+++ app.py
@@ -8,17 +8,49 @@ model2=pickle.load(open('model2.pkl','rb'))
 app = Flask(__name__)
 
 @ app.route('/')
+def index():
+    title = 'Krishi-AI'
+    return render_template('index.html', title=title)
+@ app.route('/home.html')
 def home():
     title = 'Krishi-AI'
-    return render_template('home.html', title=title)
+    return render_template('/home.html', title=title)
+@ app.route('/contact.html')
+def contact():
+    title = 'Krishi-AI Contact'
+    return render_template('contact.html', title=title)
 
-@ app.route('/crop-recommend')
-def crop_recommend():
+@ app.route('/crop.html')
+def crop():
     title = 'KrishiAI- Crop Recommendation'
     return render_template('crop.html', title=title)
 
-@ app.route('/fertilizer')
-def fertilizer_recommendation():
+@ app.route('/insecticide.html')
+def insecticide():
+    title = 'KrishiAI- Insecticide'
+    return render_template('insecticide.html', title=title)
+
+@ app.route('/predict.html')
+def predict():
+    title = 'KrishiAI- Prediction'
+    return render_template('predict.html', title=title)
+@ app.route('/predict1.html')
+def predict1():
+    title = 'KrishiAI- Prediction'
+    return render_template('predict1.html', title=title)
+
+@ app.route('/predict2.html')
+def predict2():
+    title = 'KrishiAI- Prediction'
+    return render_template('predict2.html', title=title)
+
+@ app.route('/service.html')
+def service():
+    title = 'KrishiAI- Service'
+    return render_template('service.html', title=title)
+
+@ app.route('/fertilizer.html')
+def fertilizer():
     title = ' KrishiAI- Fertilizer Suggestion'
 
     return render_template('fertilizer.html', title=title)
