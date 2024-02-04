38d7d1b819ce139ffd5bdfbc22a3b4d52831771c
Shubham <shubhamthapa2022@gmail.com>
2024-02-05 00:42:39 +0545

    third

--- app.py
+++ app.py
@@ -55,53 +55,62 @@ def fertilizer():
 
     return render_template('fertilizer.html', title=title)
 
-@app.route('/predict.html',methods=['POST'])
+@app.route('/predict.html', methods=['POST'])
 def predict_crop():
-    Nitrogen=int(request.form.get('Nitrogen'))
-    Phosphorous=int(request.form.get('Phosphorous'))
-    Potassium=int(request.form.get('Potassium'))
-    Temperature=float(request.form.get('Temperature'))
-    Humidity=float(request.form.get('Humidity'))
-  
-    PH=float(request.form.get('PH'))
-    Rainfall=float(request.form.get('Rainfall'))
-    result=model.predict(np.array([Nitrogen,Phosphorous,Potassium,Temperature,Humidity,PH,Rainfall]).reshape(1,7))
-    
-    #return str(result)
-    return render_template('predict.html', prediction=result )
+    if request.method == 'POST':
+        # Retrieve form values with default value of 0 if not provided
+        Nitrogen = int(request.form.get('Nitrogen', 0))
+        Phosphorus = int(request.form.get('Phosphorus', 0))
+        Potassium = int(request.form.get('Potassium', 0))
+        Temperature = float(request.form.get('Temperature', 0.0))
+        Humidity = float(request.form.get('Humidity', 0.0))
+        PH = float(request.form.get('PH', 0.0))
+        Rainfall = float(request.form.get('Rainfall', 0.0))
 
-#for fertilizer recomendation
-
-@app.route('/predict1.html',methods=['POST'])
-def predict_fertilizer():
-    Nitrogen=int(request.form.get('Nitrogen'))
-    Phosphorous=int(request.form.get('Phosphorous'))
-    Potassium=int(request.form.get('Potassium'))
-    Temperature=float(request.form.get('Temperature'))
-    Humidity=float(request.form.get('Humidity'))
-    Moisture=float(request.form.get('Moisture'))
-    SoilType=int(request.form.get('SoilType'))
-    CropType=int(request.form.get('CropType'))
-
-    
-
-    result=model2.predict(np.array([Temperature,Humidity,Moisture,SoilType,CropType,Nitrogen,Potassium,Phosphorous]).reshape(1,8))
-    if result[0] == 0:
-     return str("10-26-26")
-    elif result[0] ==1:
-     return str("14-35-14")
-    elif result[0] == 2:
-     return str("17-17-17	")
-    elif result[0] == 3:
-     return str("20-20")
-    elif result[0] == 4:
-     return str("28-28")
-    elif result[0] == 5:
-     return str("DAP")
+        result = model.predict(np.array([Nitrogen, Phosphorus, Potassium, Temperature, Humidity, PH, Rainfall]).reshape(1, 7))
+        print("Predicted Result:", result)
+        return render_template('predict.html', value=result[0])
     else:
-     return str("Urea")
-    
-    return render_template('fertilizer.html', prediction=result)
+        return render_template('try_again.html', message='Invalid request method')
 
-if __name__=='__main__':
+
+# for fertilizer recomendation
+
+@app.route('/predict1.html', methods=['POST'])
+def predict_fertilizer():
+    if request.method == 'POST':
+        Nitrogen = int(request.form.get('Nitrogen', 0))
+        Phosphorous = int(request.form.get('Phosphorus', 0))
+        Potassium = int(request.form.get('Potassium', 0))
+        Temperature = float(request.form.get('Temperature', 0.0))
+        Humidity = float(request.form.get('Humidity', 0.0))
+        Moisture = float(request.form.get('Moisture', 0.0))
+        SoilType = int(request.form.get('SoilType', 0))
+        CropType = int(request.form.get('CropType', 0))
+
+        result1 = model2.predict(np.array([Temperature, Humidity, Moisture, SoilType, CropType, Nitrogen, Potassium, Phosphorous]).reshape(1, 8))
+
+        predicted_fertilizer = ""
+        if result1[0] == 0:
+            predicted_fertilizer = "10-26-26"
+        elif result1[0] == 1:
+            predicted_fertilizer = "14-35-14"
+        elif result1[0] == 2:
+            predicted_fertilizer = "17-17-17"
+        elif result1[0] == 3:
+            predicted_fertilizer = "20-20"
+        elif result1[0] == 4:
+            predicted_fertilizer = "28-28"
+        elif result1[0] == 5:
+            predicted_fertilizer = "DAP"
+        else:
+            predicted_fertilizer = "Urea"
+
+        return render_template('predict1.html', str=predicted_fertilizer)
+    else:
+        return render_template('try_again.html', message='Invalid request method')
+
+
+
+if __name__ == '__main__':
     app.run(debug=True)
